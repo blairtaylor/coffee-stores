c9703dbfddcbbc63b9695251c3e541ea801c1ce0
Blair Taylor <blair.taylor@gmail.com>
2022-04-26 20:10:07 -0400

    Added comments to utility functions

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,9 @@
+// utility functions
+
+// check if array is empty
 export const isEmpty = (obj) => {
   return obj && Object.keys(obj).length === 0;
 };
 
+// return fetcher for swr
 export const fetcher = (url) => fetch(url).then((res) => res.json());
